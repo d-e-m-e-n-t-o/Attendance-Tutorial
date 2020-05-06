370683fe12f9a3c6775ff6a99e5b5d17eda9cd1a
sasaki <d.e.m.e.n.t.o@i.softbank.jp>
2020-05-06 07:06:09 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページ毎にタイトルを返すメソッド
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty? # empty?メソッドは指定した値(page_name)が無い場合ifの処理を実行、ある場合はelseの処理を実行
+      base_title
+    else
+      page_name + "|" + base_title
+    end
+  end
 end
