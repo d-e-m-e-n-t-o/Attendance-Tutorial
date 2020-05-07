8037334d319f476fe7c0205dd82f21171036b395
sasaki <d.e.m.e.n.t.o@i.softbank.jp>
2020-05-07 09:09:15 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      # flashにsuccessというハッシュキーを持たせて
+      redirect_to @user # redirect_to user_url(@user)と同じ、省略しているだけ
+    else
+      render :new
+    end
+  end
+  
+  private
+  # 下記のuser_paramsメソッドはUsersコントローラの内部でのみ実行出来ればよいので、
+  # privateキーワードを用いて外部からは使用できないようにする。
+  # 悪質な攻撃によりアプリケーションのデータベースが書き換えられないように対策を取ることが目的
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    # paramsハッシュでは:userキーを必須とし:name, :email, :password, :password_confirmation
+    # をそれぞれ許可しそれ以外は許可しない。:userキーがない場合はエラーになる。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  resources :users
+  # resourcesは指定したテーブルにに対して複数のルーティングが定義されます
+  #4つの基本操作（POST GET PATCH DELETE）に対してルーティングが設定される。
+  #超絶便利！！！！！！！！！！！！
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
